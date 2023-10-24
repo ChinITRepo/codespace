4f6eb0f85cc09d7cc3a086bb2e507dc2859f688d
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-10-24 21:01:06 +0000

    Tue, Oct 24, 2023, 9:01 PM +00:00

--- Proj1/mario.c
+++ Proj1/mario.c
@@ -3,8 +3,14 @@
 
 int main(void)
 {
-    int n;
     
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n >1);
+
     for (int i = 0; i < n ; i++)
     {
         for (int j = 0; j < n ; j++)
