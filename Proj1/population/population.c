eb034b572c541ec04686693a21c702274210c143
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-10-24 22:27:20 +0000

    Tue, Oct 24, 2023, 10:27 PM +00:00

--- Proj1/population/population.c
+++ Proj1/population/population.c
@@ -1,25 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
 
-//function starts
+// Function prototypes
 long get_start_size(void);
 long get_end_size(void);
 long calculate_years(long start, long end);
 void print_results(long start, long end, long years);
 
-//main function
+// Main function
 int main(void)
 {
-    // TODO: Prompt for start size
+    // Prompt for start size
     long n1 = get_start_size();
 
-    // TODO: Prompt for end size
+    // Prompt for end size
     long n2 = get_end_size();
 
-    // TODO: Calculate number of years until we reach threshold
+    // Calculate number of years until we reach threshold
     long n3 = calculate_years(n1, n2);
 
-    // TODO: Prlongnumber of years
+    // Print number of years
     print_results(n1, n2, n3);
 }
 
@@ -30,7 +30,7 @@ long get_start_size(void)
     {
         n = get_long("Start size: ");
     }
-    while (n >= 9);
+    while (n < 9);
     return n;
 }
 
@@ -39,7 +39,7 @@ long get_end_size(void)
     long n;
     do
     {
-        n = get_long("END size: ");
+        n = get_long("End size: ");
     }
     while (n <= 0);
     return n;
@@ -47,17 +47,17 @@ long get_end_size(void)
 
 long calculate_years(long start, long end)
 {
-    for (long i = 0; start <= end; i++)
+    long i;
+    for (i = 0; start < end; i++)
     {
-        long count = start;
-        long gain = count / 3;
-        long loss = count / 4;
-        long count = start + (gain - loss);
+        long gain = start / 3;
+        long loss = start / 4;
+        start = start + (gain - loss);
     }
     return i;
 }
 
 void print_results(long start, long end, long years)
 {
-    printf("Start size: %li\nEnd size: %li\nYears: %li\n")
+    printf("Start size: %li\nEnd size: %li\nYears: %li\n", start, end, years);
 }
