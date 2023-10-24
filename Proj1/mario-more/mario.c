23943b5ffaaa23c4d171dec43288430f8740338b
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-10-24 23:22:13 +0000

    Tue, Oct 24, 2023, 11:22 PM +00:00

--- Proj1/mario-more/mario.c
+++ Proj1/mario-more/mario.c
@@ -1,17 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Function prototypes
+int userinput(void);
+void pyramid_create(int size);
+
 int main(void)
 {
     //TODO: Get user input, int 1-8
     int n = userinput();
+
     //TODO: Define pyramid
     pyramid_create(n);
-
-    //TODO: Print pyramid
-    //expected: # #
-    // # #
-    //## ##
 }
 
 int userinput(void)
@@ -21,15 +21,37 @@ int userinput(void)
     {
         n = get_int("Pyramid size (1-8): ");
     }
-    while (n > 0 || n < 9);
+    while (n <= 0 || n > 8);  // Corrected condition
     return n;
 }
 
 void pyramid_create(int size)
 {
-    for (int i = 0; i < size; i++)
+    // Loop through each level of the pyramid
+    for (int i = 1; i <= size; i++)
     {
-        printf("#");
+        // Print spaces before #
+        for (int j = 0; j < size - i; j++)
+        {
+            printf(" ");
+        }
 
+        // Print # for the left pyramid
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+
+        // Print two spaces in the middle
+        printf("  ");
+
+        // Print # for the right pyramid
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+
+        // Move to the next line
+        printf("\n");
     }
 }
