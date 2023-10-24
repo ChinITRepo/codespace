4f8ba72bebe56c450ce86cf27a4722c05780dffd
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-10-24 21:39:22 +0000

    Tue, Oct 24, 2023, 9:39 PM +00:00

--- Proj1/population/population.c
+++ Proj1/population/population.c
@@ -4,18 +4,21 @@
 //function starts
 long get_start_size(void);
 long get_end_size(void);
-void print_grid(int n);
+void print_years(int y);
 
 //main function
 int main(void)
 {
     // TODO: Prompt for start size
+    n1 = get_start_size();
 
     // TODO: Prompt for end size
+    n1 = get_end_size();
 
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
+    print_years(y)
 }
 
 int get_start_size(void)
@@ -24,6 +27,17 @@ int get_start_size(void)
     do
     {
         n = get_long("Start size: ");
+    }
+    while (n =< 9);
+    return n;
+}
+
+int get_end_size(void)
+{
+    int n;
+    do
+    {
+        n = get_long("END size: ");
     }
     while (n =< 9);
     return n;
