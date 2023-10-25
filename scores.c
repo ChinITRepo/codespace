fd5a99e3d301f025d1bddca4a70483979466e27b
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-10-25 23:51:19 +0000

    Wed, Oct 25, 2023, 11:51 PM +00:00

--- scores.c
+++ scores.c
@@ -6,5 +6,5 @@ int main(void)
     int score2 = 73;
     int score3 = 33;
 
-    printf("Average: %f\n", ((float) score1 + (float) score2 + (float) score3));
+    printf("Average: %f\n", ((float) score1 + (float) score2 + (float) score3) \ 3);
 }
