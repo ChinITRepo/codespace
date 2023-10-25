b1fe71dfd9a0999b65a7c3efc0cac9627eb44696
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-10-25 23:59:28 +0000

    Wed, Oct 25, 2023, 11:59 PM +00:00

--- scores.c
+++ scores.c
@@ -2,11 +2,10 @@
 
 int main(void)
 {
-    int scores[3];
+    int scores[3]
     int scores[0] = 72;
     int scores[1] = 73;
     int scores[2] = 33;
 
-    for 
-    printf("Average: %i\n", ((float) scores[] \ 3));
+    printf("Average: %f\n", ((float) scores[0] + (float) scores[1] + (float) scores[2]) \ (float) 3);
 }
