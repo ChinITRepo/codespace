1b4a76cafeeaca783ab143152ecb24892d6399a1
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-10-26 01:19:55 +0000

    Thu, Oct 26, 2023, 1:19 AM +00:00

---
+++ scoresgpt.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <cs50.h>
+
+// Function prototypes
+int* scores(int n);
+float average(int n, int scores[]);
+
+int main(void)
+{
+    int j = get_int("How many scores would you like to average? ");
+    int *scoreArray = scores(j);
+    printf("Average: %f\n", average(j, scoreArray));
+}
+
+// Function to get scores
+int* scores(int n)
+{
+    // Dynamically allocate memory for scores
+    int *scores = malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        do
+        {
+            scores[i] = get_int("Score: ");
+        } while (scores[i] < 1); // Assuming you only want scores greater than or equal to 1
+    }
+    return scores;
+}
+
+// Function to calculate average
+float average(int n, int scores[])
+{
+    float sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += scores[i];
+    }
+    return sum / n;
+}
