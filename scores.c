2d7d36424be2d2892a5e95791c872533d6fc5cfb
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-10-28 19:23:47 +0000

    Sat, Oct 28, 2023, 7:23 PM +00:00

--- scores.c
+++ scores.c
@@ -2,19 +2,20 @@
 #include <cs50.h>
 
 float average(int array[])
+int get_scores(int tests, )
+
 int main(void)
 {
-    int scores[3]
-    for (int i = 0; 1 < 3; i++)
-    {
-        scores[i] = get_int("Score: ")
-    }
+
     printf("Average: %f\n", average);
 }
 
 float average(int array[])
 {
-    
+    for (int i = 0; )
+    {
+
+    }
 
 }
 
