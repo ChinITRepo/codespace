93cb3e3c552fcb524413dac19f341a7c8c4b6360
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-10-29 11:22:46 +0000

    Sun, Oct 29, 2023, 11:22 AM +00:00

--- CS50/week 2/scrabble/scrabble.c
+++ CS50/week 2/scrabble/scrabble.c
@@ -1,10 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int main(void)
+{
 
+}
+
+int score(int argc, string argv[])
+{
+
+}
 
 //1	3	3	2	1	4	2	4	1	8	5	1	3	1	1	3	10	1	1	1	1	4	4	8	4	10
 //For example, if we wanted to score the word “CODE”, we would note that the ‘C’ is worth 3 points, the ‘O’ is worth 1 point, the ‘D’ is worth 2 points, and the ‘E’ is worth 1 point. Summing these, we get that “CODE” is worth 7 points.
 
 //implement a program in C that determines the winner of a short Scrabble-like game. Your program should prompt for input twice: once for “Player 1” to input their word and once for “Player 2” to input their word. Then, depending on which player scores the most points, your program should either print “Player 1 wins!”, “Player 2 wins!”, or “Tie!” (in the event the two players score equal points).
+
 
