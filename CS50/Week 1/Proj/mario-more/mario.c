9a1a203b148ccdfdb93a559a87c94f5ef5a5117e
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-10-31 22:34:12 +0000

    Tue, Oct 31, 2023, 10:34 PM +00:00

--- CS50/Week 1/Proj/mario-more/mario.c
+++ CS50/Week 1/Proj/mario-more/mario.c
@@ -29,17 +29,19 @@ void pyramid_create(int size)
 {
     for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < i; j++)
-        {
-            printf("#");
-        }
-
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < size - 1)
         {
             printf(" ");
         }
 
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+
+            printf(" ");
+
+        for (int j = 0; j < i + 1; j++)
         {
             printf("#");
         }
