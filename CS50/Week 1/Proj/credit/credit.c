bd806c288998841c6cd6369eba790387e1d406ad
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-11-04 10:54:57 +0000

    Sat, Nov 4, 2023, 10:54 AM +00:00

--- CS50/Week 1/Proj/credit/credit.c
+++ CS50/Week 1/Proj/credit/credit.c
@@ -1,28 +1,2 @@
 #include <stdio.h>
 #include <cs50.h>
-
-int length(int card_number);
-void luhn(int card_number);
-
-int main(void)
-{
-
-}
-
-int length(int card_number)
-{
-    for (i = 0; i > )
-    {
-
-    }
-}
-
-void luhn(int card_number,int length)
-{
-
-    switch(card_number)
-    {
-
-    }
-}
-
