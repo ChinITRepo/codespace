42186c54bf63adb6549b86594a4872e69655e6f9
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-11-07 20:36:17 +0000

    Tue, Nov 7, 2023, 8:36 PM +00:00

--- CS50/week 2/proj/readability/readability.c
+++ CS50/week 2/proj/readability/readability.c
@@ -10,16 +10,6 @@ int main(void)
 {
     string text = get_string("Text: ");
     printf("%s", text);
-
-    for (int i = 0; i > strlen(text); i++)
-    {
-        for (int j = 0; j > strlen(text[i]); j++)
-        {
-
-
-        }
-
-    }
 }
 
 //index = 0.0588 * L - 0.296 * S - 15.8
