8a4474d1120dafdf4111efd59c7289268d01fe0c
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-11-08 21:51:22 +0000

    Wed, Nov 8, 2023, 9:51 PM +00:00

--- CS50/week 2/proj/readability/readability/readability.c
+++ CS50/week 2/proj/readability/readability/readability.c
@@ -1,14 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
 
 int main(void)
 {
-
+    string text = get_string("Test: ");
+    count_letters(text);
 }
 
-int count_letters(char *s)
+int count_letters(string text)
 {
-    int length = strlen(s);
+    int length = strlen(text);
 
     for (i = 0; i < length; i++)
     {
