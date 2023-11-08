88b6f83140faa29ca84919c8867958973685a603
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-11-08 22:37:05 +0000

    Wed, Nov 8, 2023, 10:37 PM +00:00

--- CS50/week 2/proj/readability/readability/readability.c
+++ CS50/week 2/proj/readability/readability/readability.c
@@ -6,9 +6,10 @@ int count_letters(string text);
 int count_words(string text);
 int count_sentences(string text);
 
+
 int main(void)
 {
-    string text = get_string("Test: ");
+    string text = get_string("Text: ");
     int letters = count_letters(text);
     int words = count_words(text);
     int sentences = count_sentences(text);
