0530b095a9e2396ea50d10fe7b1bafdb8825e380
ChinITRepo <148910417+ChinITRepo@users.noreply.github.com>
2023-11-10 20:08:10 +0000

    Fri, Nov 10, 2023, 8:08 PM +00:00

--- CS50/week 2/proj/readability/readability/readability.c
+++ CS50/week 2/proj/readability/readability/readability.c
@@ -3,14 +3,16 @@
 #include <string.h>
 #include <math.h>
 
+string get_text(void);
 int count_letters(string text);
 int count_words(string text);
 int count_sentences(string text);
 int coleman_Liau_index(int letters, int words, int sentences);
+void return_grade(int grade)
 
 int main(void)
 {
-    string text = get_string("Text: ");
+    string text = get_text();
     int letters = count_letters(text);
     int words = count_words(text);
     int sentences = count_sentences(text);
@@ -92,5 +94,9 @@ int coleman_Liau_index(int letters, int words, int sentences)
     printf("average Sen: %f\n", average_sentences);
     float index = 0.0588 * average_letters - 0.296 * average_sentences - 15.8;
     return (int) round(index);
+}
 
+string get_text(void)
+{
+    string text = get_string("Text: ");
 }
